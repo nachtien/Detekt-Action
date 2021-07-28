8150f7c9dd1c8d67ba96edc457de0d53c88e328a
Nick Achtien <nachtien@users.noreply.github.com>
2021-07-28 15:49:11 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,9 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 java -jar /opt/detekt.jar --fail-fast --config "${INPUT_DETEKT_CONFIG}" \
   --report xml:detekt_report.xml \
+  --baseline "${INPUT_DETEKT_BASELINE}" \
   --excludes "${INPUT_DETEKT_EXCLUDES}" \
-  --plugins /opt/detekt-formatting.jar \
-  -b "${INPUT_DETEKT_BASELINE}"
+  --plugins /opt/detekt-formatting.jar
 
 reviewdog -f=checkstyle -name="detekt" -reporter="${INPUT_REVIEWDOG_REPORTER}" \
   -level="${INPUT_REVIEWDOG_LEVEL}" -filter-mode="${INPUT_REVIEWDOG_FILTER}" <detekt_report.xml
