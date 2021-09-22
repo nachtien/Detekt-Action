343c80cb07f3e2ff114020527cc5ba9358a35619
Nick Achtien <nachtien@users.noreply.github.com>
2021-09-22 11:06:32 -0500

    add config back

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,9 @@ if [ "$INPUT_FAIL_ON_ERROR" = true ] ; then
   set -o pipefail
 fi
 
-set -x
-
-#echo "baseline: "
-#echo "$(<"${INPUT_DETEKT_BASELINE}")"
-#cat "${INPUT_DETEKT_BASELINE}"
-
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-java -jar /opt/detekt.jar --fail-fast --debug --config config/detekt/config.xml \
+java -jar /opt/detekt.jar --fail-fast --config "${INPUT_DETEKT_CONFIG}" \
   --report xml:detekt_report.xml \
   --baseline baseline.xml \
   --excludes "${INPUT_DETEKT_EXCLUDES}" \
